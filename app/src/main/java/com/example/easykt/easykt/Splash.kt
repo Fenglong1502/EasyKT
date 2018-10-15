135c79783a1fd8c88b202d663e2abe0e863cbd36
Feng Long <fenglong1502@Hotmail.com>
2018-10-15 20:34:53 +0800

    no message

---
+++ app/src/main/java/com/example/easykt/easykt/Splash.kt
@@ -0,0 +1,48 @@
+package com.example.easykt.easykt
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.content.ContextCompat.startActivity
+import com.example.easykt.easykt.R.layout.activity_main
+
+class Splash : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, activity_main::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
+
+}
