9d091f18742be30e447161ccb342095845d027d1
Feng Long <fenglong1502@Hotmail.com>
2018-10-15 21:08:29 +0800

    no message

---
+++ app/src/main/java/com/example/easykt/easykt/ActivitQuiz.kt
@@ -0,0 +1,12 @@
+package com.example.easykt.easykt
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitQuiz : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activit_quiz)
+    }
+}
---
+++ app/src/main/java/com/example/easykt/easykt/ActivityTutorial.kt
@@ -0,0 +1,12 @@
+package com.example.easykt.easykt
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTutorial : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+    }
+}
