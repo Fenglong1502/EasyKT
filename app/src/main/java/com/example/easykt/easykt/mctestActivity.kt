bb9404868f2a5658ad6fee6bfe5d43db87efa0da
MC <gaimc@u.nus.edu>
2018-10-23 08:51:42 +0800

    added function to move to different activity in cardview

--- app/src/main/java/com/example/easykt/easykt/mctestActivity.kt
+++ app/src/main/java/com/example/easykt/easykt/mctestActivity.kt
@@ -1,12 +1,32 @@
 package com.example.easykt.easykt
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.CardView
 
 class mctestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mctest)
+
+        val quizCard = findViewById<CardView>(R.id.quizCard)
+        quizCard.setOnClickListener {
+            val myIntent = Intent(this, ActivitQuiz::class.java)
+            startActivity(myIntent)
+        }
+
+        val tutorialCard = findViewById<CardView>(R.id.tutorialCard)
+        tutorialCard.setOnClickListener {
+            val myIntent = Intent(this, ActivityTutorial::class.java)
+            startActivity(myIntent)
+        }
+
+        val videoCard = findViewById<CardView>(R.id.videoCard)
+        videoCard.setOnClickListener {
+            val myIntent = Intent(this, ActivityVideo::class.java)
+            startActivity(myIntent)
+        }
     }
 }
