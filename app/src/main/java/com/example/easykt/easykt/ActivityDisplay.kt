fc49717746c174e82d8cd59d8d4405ccbc39decf
Feng Long <fenglong1502@Hotmail.com>
2018-10-30 13:45:55 +0800

    Deleted unneccesary activities

--- app/src/main/java/com/example/easykt/easykt/ActivityDisplay.kt
+++ app/src/main/java/com/example/easykt/easykt/ActivityDisplay.kt
@@ -6,7 +6,7 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
-import kotlinx.android.synthetic.main.activity_fltest2.*
+import kotlinx.android.synthetic.main.activity_display.*
 
 class ActivityDisplay : AppCompatActivity() {
 
